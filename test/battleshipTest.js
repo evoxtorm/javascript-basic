4cb981480a9ee996d62b8bcf566e4e0037e7cec0
Hitesh Joshi <evostorm96@gmail.com>
2018-02-06 20:09:50 +0530

    Update battleshipTest.js

--- test/battleshipTest.js
+++ test/battleshipTest.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var thingToTest = require('../battleship.js');
 
-describe('Array', function() {
+describe('Array', () => {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
